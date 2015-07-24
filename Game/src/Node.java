05c51a86869d36335fcbd5c918532b735e18dd88
liyin <liyin@LIYIN-CN.cn.oracle.com>
2015-07-24 13:15:37 +0800

    dasfasf

--- Game/src/Node.java
+++ Game/src/Node.java
@@ -1,11 +1,11 @@
 
-
 class Node<E> {
-    E data;
-    Node<E> next;
-//testdfadsf
-    Node(E element, Node<E> next) {
-        this.data = element;
-        this.next = next;
-    }
+	E data;
+	Node<E> next;
+
+	// testdfadsf
+	Node(E element, Node<E> next) {
+		this.data = element;
+		this.next = next;
+	}
 }
--- Game/src/Power.java
+++ Game/src/Power.java
@@ -1,15 +1,17 @@
-
 public class Power {
-	public static double myPower(double base, int e){
+	public static double myPower(double base, int e) {
 		double result = 0.0d;
-		if(e == 0.0d) return 1.0d;
-		if(e == 1.0d) return base;
-		if(e >1.0d)
+		if (e == 0.0d)
+			return 1.0d;
+		if (e == 1.0d)
+			return base;
+		if (e > 1.0d)
 			result = myPower(base, e >> 1);
-		    result *= result;
-		if(e%2 == 1.0d) result *= base;
-		if( e > -0.000001 && e < 0.000001 )
-			result = 1.0/result;
+		result *= result;
+		if (e % 2 == 1.0d)
+			result *= base;
+		if (e > -0.000001 && e < 0.000001)
+			result = 1.0 / result;
 		return result;
 	}
 
